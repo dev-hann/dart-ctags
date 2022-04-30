eb8cc06dd9b5a474872488afbd80d6d43ffa0cb5
dev-hann <developer.hann@gmail.com>
2022-04-30 14:40:12 +0800

    add mixin declaration

--- bin/dart_ctags.dart
+++ bin/dart_ctags.dart
@@ -50,19 +50,3 @@ void main(List<String> arguments) async {
     File(opts.output!).writeAsString(_res);
   }
 }
-
-class A {}
-
-class B {}
-
-class C {}
-
-class D extends A {}
-
-class E extends B implements C {}
-
-class F extends A with C implements D {}
-
-mixin ZZ {}
-
-mixin KK on A implements C {}
